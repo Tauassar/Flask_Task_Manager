d0f06715bf0a512bb5f432e1aab07b4d27858b1c
Tauassar <tauassar.tatiyev@nu.edu.kz>
2021-11-09 16:02:07 +0600

    added css & added feature to add posts

--- blog.py
+++ blog.py
@@ -1,5 +1,6 @@
 # blog.py controller
 from flask import Flask, render_template, request, session, flash, redirect, url_for, g
+from functools import wraps
 
 import sqlite3
 
@@ -15,11 +16,22 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 
+def login_required(test):
+    @wraps(login)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash("You are not authorized access this page")
+            return redirect(url_for('login'))
+    return wrap
+
+
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'], endpoint='login')
 def login():
     error = None
     status_code = 200
@@ -33,16 +45,38 @@ def login():
     return render_template('login.html'), status_code
 
 
-@app.route('/logout', methods=['GET'])
+@app.route('/logout', methods=['GET'], endpoint='logout')
 def logout():
     session.pop('logged_in', None)
     flash('Logged out')
     return redirect(url_for('login'))
 
 
-@app.route('/main')
+@app.route('/main', endpoint='main')
+@login_required
 def main():
-    return render_template('main.html')
+    g.db = connect_db()
+    cur = g.db.execute('SELECT * from posts')
+    posts = [dict(title=row[0], post=row[1]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template('main.html', posts=posts)
+
+
+@app.route('/add', methods=['POST'], endpoint='add')
+@login_required
+def add():
+    title = request.form['title']
+    post = request.form['post']
+    if not title or not post:
+        flash("All fields are required. Please try again")
+        return redirect(url_for('main'))
+    else:
+        g.db = connect_db()
+        g.db.execute('INSERT INTO posts (title, post) values (?,?)', [request.form['title'], request.form['post']])
+        g.db.commit()
+        g.db.close()
+        flash('New post successfully added')
+        return redirect(url_for('main'))
 
 
 if __name__ == '__main__':
